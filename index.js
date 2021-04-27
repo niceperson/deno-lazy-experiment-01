cefb5153348aa9c4cf6c2e4accffaa8765492e55
Syukri <niceperson404@gmail.com>
2021-04-27 08:01:14 +0800

    use pgp instead

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import { serve } from "https://deno.land/std@0.95.0/http/server.ts";
-const s = serve({ port: 8000 });
-console.log("http://localhost:8000/");
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
-}
+import pogo from 'https://deno.land/x/pogo/main.ts';
+
+const server = pogo.server({ port : 3000 });
+
+server.router.get('/', () => {
+    return 'Hello, world!';
+});
+
+server.start();
